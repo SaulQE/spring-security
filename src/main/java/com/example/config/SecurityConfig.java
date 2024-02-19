11f5de2c4c1b1dc90fe921e5aec1527bbf932fbd
SaulQE <saul.esquivel.028@gmail.com>
2024-02-18 19:51:04 -0500

    sessions controller

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -20,11 +23,24 @@ public class SecurityConfig {
                     auth.anyRequest().authenticated(); // Para cualquier otro endpoint debe estar auth
                 })
                 .formLogin(formLogin -> {
-                    formLogin.successHandler(successHandler());
+                    formLogin.successHandler(successHandler()); // URL a donde se va a ir despues de iniciar sesion
                     formLogin.permitAll();
+                })
+                .sessionManagement(sesMan -> {
+                    sesMan.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+                    sesMan.invalidSessionUrl("/login"); // Hasta que no se auth correctamente
+                    sesMan.maximumSessions(1).expiredUrl("/login").sessionRegistry(sessionRegistry());
+                    sesMan.sessionFixation()
+                            .migrateSession(); //migrateSession - newSession - none
                 })
                 .build();
     }
+
+    @Bean
+    public SessionRegistry sessionRegistry(){
+        return new SessionRegistryImpl();
+    }
+
 
     public AuthenticationSuccessHandler successHandler(){
         return ((request, response, authentication) -> {
